654fe473e03b6a4bfdaf59757f1721306b3bde85
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-12-11 11:31:20 -0500

    Corrección de bug. Close #2

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,22 +5,22 @@ import DataCard from './DataCard'
 function Card() {
 
   return (
-    <h1>hola mundo</h1>
     <>
-    { DataCard.map(cards => (
+      <h1>hola mundo</h1>
+      { DataCard.map(cards => (
 
-<div className="card my-4 mx-4"
-key={cards.id}
-style={{width: "18rem"}}>
-  <img src={cards.img} className="card-img-top" alt="..." />
-  <div className="card-body">
-  <h5 className="card-title">{cards.title}</h5>
-    <p className="card-text">{cards.description}</p>
-    <a href="!#" className="btn btn-primary">{cards.btn}</a>
-  </div>
-</div>
-))};
-</>
+        <div className="card my-4 mx-4"
+          key={cards.id}
+          style={{ width: "18rem" }}>
+          <img src={cards.img} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{cards.title}</h5>
+            <p className="card-text">{cards.description}</p>
+            <a href="!#" className="btn btn-primary">{cards.btn}</a>
+          </div>
+        </div>
+      ))};
+    </>
   )
 }
 
